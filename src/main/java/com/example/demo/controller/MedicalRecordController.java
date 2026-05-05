70cc19d0b208b8c5895a041a1cff53103653e80c
Yuvashree R <yuvashree.ra05@gmail.com>
2026-05-05 21:38:47 +0530

    Update MedicalRecordController.java

--- src/main/java/com/example/demo/controller/MedicalRecordController.java
+++ src/main/java/com/example/demo/controller/MedicalRecordController.java
@@ -10,7 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
