3bd3864347758862078b3e56f78c14231fe0935c
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-01 09:36:33 +0530

    Remove unused import statement in AuditController

--- src/main/java/com/example/demo/controller/AuditController.java
+++ src/main/java/com/example/demo/controller/AuditController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-
+//import
 import com.example.demo.entity.AuditLog;
 import com.example.demo.repository.AuditLogRepository;
 import org.springframework.data.domain.Sort;
