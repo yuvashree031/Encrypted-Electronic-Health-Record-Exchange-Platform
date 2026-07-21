602143fe72f8532aa777a5912b8a67d6d577fcee
Yuvashree R <yuvashree.ra05@gmail.com>
2026-07-21 11:57:40 +0530

    Update AuditController.java

--- src/main/java/com/example/demo/controller/AuditController.java
+++ src/main/java/com/example/demo/controller/AuditController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-
+//imp
 import com.example.demo.entity.AuditLog;
 import com.example.demo.repository.AuditLogRepository;
 import org.springframework.data.domain.Sort;
